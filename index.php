41a8a22450504eea785c9ef5ca7c8a6a6107351a
jashanpreet12345 <133545583+jashanpreet12345@users.noreply.github.com>
2023-08-09 10:31:15 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+<title>HANDMADE JEWELRY</title>
+  <meta name="author" content="Handmade jewelry"/>
+  <meta name="description" content="There is no standard look for handmade jewelry"/>
+  <link rel="shortcut icon" href="images/jewelry.jpg" type="image/x-icon" />
+  <link rel="stylesheet" href="css/styles.css" type="text/css"/>
+  
+  <script type="text/javascript">
+alert("Welcome to the project of Handmade Jewelry");
+</script>
+  
+  </head>
+  
+  <body>
+    <!--Page Header-->
+    <header>
+      
+    <h1>HANDMADE JEWELRY</h1>
+
+      <?php
+	include("includes/global-nav.php");
+?>
+<br>
+<p>Handmade jewelry is made by an artist without the use of mass-manufacturing machines. There is no standard "look" for handmade jewelry. It can range from simple hand-assembled pendants and bracelets to intricate designs that take hours or days and skilled techniques to finish. What they all have in common though is that a jewelry artist makes each individual piece using only their hands and simple tools.</p>
+
+ </header>
+ <!--Page Main--> 
+  <main>  
+<p>When you choose to purchase a piece of handmade jewelry, you're going to get a unique keepsake each time. There is almost always a little variation between each individual piece of the same style, due to the nature of handmaking anything. There may be slight imperfections - maybe a cut line isn't 100% straight - but there is also much more character to a piece of handmade jewelry. There's a story behind not only the design of the jewelry piece but the artist you're buying from.</p>
+<nav>
+     <img id="img1" src="images/hj.jpg" height="400" width="400" alt="hj.jpg">
+
+    <img id="img2" src="images/hj2.jpg" height="400" width="400" alt="hj2.jpg">
+
+    <img id="img3" src="images/hj3.jpg" height="400" width="400" alt="hj3.jpg">
+</nav>
+  <p>You're helping a small business owner to pursue their passion when you buy handmade jewelry. Some of these businesses are as small as a stay-at-home mom working from home and selling items on Etsy. Most are small business owners who are active in their local community beyond just the economy. They also often source from other small business suppliers for their materials and stones. Even if you're not in their local community and purchase online, you're supporting the small business backbone of our economy.
+</p>
+  </main>
+
+    
+    <!--Page Footer-->
+<footer>
+  <h3></h3>
+  
+  <p>Handmade jewelry artists are also more likely to make pieces using ethically-sourced materials. They are usually a great source for anyone looking for ethical and sustainable jewelry. They tend to choose source companies with a reputation for recycling scrap metal or who can guarantee the conflict-free background of stones. Some jewelers will even go out to mining locations to witness the mining operations themselves. This ensures the origin of the diamonds and gemstones they buy line up with their own ethos. They may even dig around for their own stones!
+
+</p>
+
+  <?php 
+         include("includes/footer-nav.php");
+?>
+  <br>
+<small>Copyright © Handmade Jewelry</small>
+  </footer>
+    </body>
+</html>
